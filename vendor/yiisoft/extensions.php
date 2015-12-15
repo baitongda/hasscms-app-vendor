90c797356b1d1fe52757901da9b0763e255af4c1
zhepama <zhepama@gmail.com>
2015-12-16 04:38:47 +0800

    Init

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,170 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-httpclient' => 
+  array (
+    'name' => 'yiisoft/yii2-httpclient',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/httpclient' => $vendorDir . '/yiisoft/yii2-httpclient',
+    ),
+  ),
+  'yiisoft/yii2-imagine' => 
+  array (
+    'name' => 'yiisoft/yii2-imagine',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/imagine' => $vendorDir . '/yiisoft/yii2-imagine',
+    ),
+  ),
+  'yiisoft/yii2-twig' => 
+  array (
+    'name' => 'yiisoft/yii2-twig',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/twig' => $vendorDir . '/yiisoft/yii2-twig',
+    ),
+  ),
+  'fourteenmeister/dsn' => 
+  array (
+    'name' => 'fourteenmeister/dsn',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@fourteenmeister/helpers' => $vendorDir . '/fourteenmeister/dsn/src',
+    ),
+  ),
+  'creocoder/yii2-flysystem' => 
+  array (
+    'name' => 'creocoder/yii2-flysystem',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@creocoder/flysystem' => $vendorDir . '/creocoder/yii2-flysystem/src',
+    ),
+  ),
+  'creocoder/yii2-nested-sets' => 
+  array (
+    'name' => 'creocoder/yii2-nested-sets',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@creocoder/nestedsets' => $vendorDir . '/creocoder/yii2-nested-sets/src',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'yiisoft/yii2-jui' => 
+  array (
+    'name' => 'yiisoft/yii2-jui',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/jui' => $vendorDir . '/yiisoft/yii2-jui',
+    ),
+  ),
+  'yiisoft/yii2-authclient' => 
+  array (
+    'name' => 'yiisoft/yii2-authclient',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/authclient' => $vendorDir . '/yiisoft/yii2-authclient',
+    ),
+  ),
+  'dektrium/yii2-user' => 
+  array (
+    'name' => 'dektrium/yii2-user',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@dektrium/user' => $vendorDir . '/dektrium/yii2-user',
+    ),
+    'bootstrap' => 'dektrium\\user\\Bootstrap',
+  ),
+  'kartik-v/yii2-krajee-base' => 
+  array (
+    'name' => 'kartik-v/yii2-krajee-base',
+    'version' => '1.8.0.0',
+    'alias' => 
+    array (
+      '@kartik/base' => $vendorDir . '/kartik-v/yii2-krajee-base',
+    ),
+  ),
+  'kartik-v/yii2-widget-select2' => 
+  array (
+    'name' => 'kartik-v/yii2-widget-select2',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@kartik/select2' => $vendorDir . '/kartik-v/yii2-widget-select2',
+    ),
+  ),
+  'yiidoc/yii2-timeago' => 
+  array (
+    'name' => 'yiidoc/yii2-timeago',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/timeago' => $vendorDir . '/yiidoc/yii2-timeago',
+    ),
+  ),
+  'vova07/yii2-imperavi-widget' => 
+  array (
+    'name' => 'vova07/yii2-imperavi-widget',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@vova07/imperavi' => $vendorDir . '/vova07/yii2-imperavi-widget/src',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'trntv/yii2-debug-xhprof' => 
+  array (
+    'name' => 'trntv/yii2-debug-xhprof',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@trntv/debug/xhprof' => $vendorDir . '/trntv/yii2-debug-xhprof',
+    ),
+    'bootstrap' => 'trntv\\debug\\xhprof\\panels\\XhprofPanel',
+  ),
+);
---
+++ vendor/yiisoft/yii2-twig/docs/guide-ja/README.md
@@ -0,0 +1,12 @@
+Yii 2 Twig エクステンション
+===========================
+
+このエクステンションは [Twig](http://twig.sensiolabs.org/) ビューテンプレートエンジンを使うことが出来るようにする `ViewRenderer` を提供します。
+
+始めよう
+--------
+
+* [インストール](installation.md)
+* [テンプレート構文](template-syntax.md)
+* [レイアウトとウィジェット](layouts-and-widgets.md)
+* [追加の構成](additional-configuration.md)
